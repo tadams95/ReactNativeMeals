550084f808e0164faae5e8960431b4d374f532e7
tadams95 <tyrelleadams28@gmail.com>
2023-12-19 11:53:47 -0800

    ReactNative Meals first commit

---
+++ components/MealItem.js
@@ -0,0 +1,80 @@
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+} from "react-native";
+
+export default function MealItem({
+  title,
+  imageUrl,
+  duration,
+  complexity,
+  affordability,
+}) {
+  return (
+    <View style={styles.mealItem}>
+      <Pressable
+        android_ripple={{ color: "#D3CEC4" }}
+        style={({ pressed }) => (pressed ? styles.buttonPress : null)}
+      >
+        <View style={styles.innerContainer}>
+          <View>
+            <Image style={styles.image} source={{ uri: imageUrl }} />
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.detailItem}>{duration} minutes</Text>
+            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+          </View>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 4,
+    overflow: "hidden",
+    backgroundColor: "white",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  image: {
+    height: 200,
+    width: "100%",
+    borderRadius: 4,
+  },
+  title: {
+    fontWeight: "500",
+    textAlign: "center",
+    fontSize: 18,
+    margin: 8,
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+  buttonPress: {
+    opacity: 0.5,
+  },
+});
---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,43 @@
+import { FlatList, StyleSheet, View } from "react-native";
+import { MEALS } from "../data/dummy-data";
+
+import MealItem from "../components/MealItem";
+
+export default function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      affordability: item.affordability,
+      complexity: item.complexity,
+    };
+
+    return <MealItem {...mealItemProps} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
